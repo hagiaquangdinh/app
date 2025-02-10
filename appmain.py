9c254b11a5a13b9c615163ade2fcb66cab6e2d15
hagiaquangdinh <hagiaquangdinh@gmail.com>
2025-02-10 14:37:00 +0700

    Update appmain.py

--- appmain.py
+++ appmain.py
@@ -1,1 +1,15 @@
 
+import streamlit as st
+from fractions import Fraction
+def main():
+    st.title("Fraction Calculator")
+    number = st.number_input("Enter a number: ",
+                             min_value= 0,
+                             max_value= 900
+                             )
+    if st.button("Calculate"):
+      result = Fraction(str(number))
+      st.write(f"The fraction of {number} is: {result}")
+
+if __name__ == "__main__":
+    main()
