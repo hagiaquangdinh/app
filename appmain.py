dda63fb37ba223aa6cc461f934e2de6f0b6d42be
hagiaquangdinh <hagiaquangdinh@gmail.com>
2025-02-10 14:46:32 +0700

    Update appmain.py

--- appmain.py
+++ appmain.py
@@ -2,6 +2,19 @@
 import streamlit as st
 from fractions import Fraction
 
+def fract(n):
+  """Tính giai thừa của số nguyên dương n."""
+
+  if n < 0:
+    return "Không thể tính giai thừa của số âm."
+  elif n == 0:
+    return 1
+  else:
+    giai_thua = 1
+    for i in range(1, n + 1):
+      giai_thua *= i
+    return giai_thua
+
 def fact():
     if n == 0 or n == 1:
         return 1
@@ -15,7 +28,7 @@ def main():
                              max_value= 900
                              )
     if st.button("Calculate"):
-      result = Fraction(number)
+      result = fract(number)
       st.write(f"The fraction of {number} is: {result}")
 
 if __name__ == "__main__":    
