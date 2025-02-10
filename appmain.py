63c973cc490926b064d2a88fcfbb04e865932422
hagiaquangdinh <hagiaquangdinh@gmail.com>
2025-02-10 14:52:29 +0700

    Update appmain.py

--- appmain.py
+++ appmain.py
@@ -27,5 +27,5 @@ n = st.number_input("Enter a number: ",
                              max_value= 900
                              )
 if st.button("Calculate"):
-   r = fact(n)
-   st.write(f"The fraction of {n} is: {r}")
+   r = fact(int(n))
+   st.write(f"The fraction of {int(n)} is: {r}")
