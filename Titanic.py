b179a503f82daa85580d78115d6f31948f876c09
hagiaquangdinh <hagiaquangdinh@gmail.com>
2025-02-13 10:00:08 +0700

    Update Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
 from sklearn.model_selection import train_test_split
 
 st.title("Titanic Data Processing and Logging with MLflow")
