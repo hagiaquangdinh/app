51af3d5ab533f76b88b4d08895c4133b29f79826
hagiaquangdinh <hagiaquangdinh@gmail.com>
2025-02-13 09:58:24 +0700

    Update Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -12,13 +12,6 @@ data = pd.read_csv(url)
 # Data cleaning (handling missing Age values)
 new_data = data.dropna(subset=['Age'])
 
-# Display some data info in Streamlit
-st.subheader("Data Overview")
-st.write(f"Original data shape: {data.shape}")
-st.write(f"Data shape after dropping NaN Age: {new_data.shape}")
-st.write(f"Mean Age: {new_data['Age'].mean()}")
-st.dataframe(new_data.head())  # Display the first few rows
-
 # Split data
 x = new_data.drop('Survived', axis=1)
 y = new_data['Survived']
@@ -26,10 +19,17 @@ y = new_data['Survived']
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=42)
 
-# MLflow setup and logging
+new_data_split = pd.DataFrame(list(new_data.items()), columns=['Data Set', 'Size'])
+
+st.title("Titanic Data Processing and Logging with MLflow")
+st.table(new_data_split)
+
+fig = px.bar(new_data_split, x='Data Set', y='Size', title='Data Split', color='Data Set')
+st.plotly_chart(fig)
+
+st.write("Data Split")
 
 
-st.write(f"MLflow Run ID: {run.info.run_uuid}") #Display the run ID
-st.write("Data and parameters logged to MLflow.")
 
-st.write("Done!")
+
+
